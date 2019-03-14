f2d99a15a2a7cee601162cb16c16d916254b3389
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-13 21:12:37 -0400

    first commit

---
+++ ListMasterApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListMasterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new PageMinhaLista());
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ListMasterApp/PageMinhaLista.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ListMasterApp
+{
+    public partial class PageMinhaLista : ContentPage
+    {
+        List<String> lista_conteudo = new List<string>(new string[] {
+
+            "Item 01",
+            "Item 02",
+            "Item 03"
+        });
+
+        public PageMinhaLista()
+        {
+            InitializeComponent();
+            minhaLista.ItemsSource = lista_conteudo;
+        }
+    }
+}
